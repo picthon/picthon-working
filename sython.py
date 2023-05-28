a4ae8e54a1e2eef984b83345740dd2895e1e0d3a
picthon <132414308+picthon@users.noreply.github.com>
2023-05-28 18:39:57 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -81,6 +81,53 @@ async def join_channel():
     except BaseException:
        pass
   
+@sython.on(events.NewMessage(outgoing=True, pattern=".اسم وقتي"))
+async def _(event):
+    if event.fwd_from:
+        return
+    while True:
+        HM = time.strftime("%I:%M")
+        for normal in HM:
+            if normal in normzltext:
+                namefont = namerzfont[normzltext.index(normal)]
+                HM = HM.replace(normal, namefont)
+        name = f"  {HM}"
+        LOGS.info(name)
+        try:
+            await sython(
+                functions.account.UpdateProfileRequest(
+                    first_name=name
+                )
+            )
+        except FloodWaitError as ex:
+            LOGS.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+        await asyncio.sleep(DEL_TIME_OUT)
+	
+	
+@sython.on(events.NewMessage(outgoing=True, pattern=".بايو وقتي"))
+async def _(event):
+    if event.fwd_from:
+        return
+    while True:
+        HM = time.strftime("%H:%M")
+        for normal in HM:
+            if normal in normzltext:
+                namefont = namerzfont[normzltext.index(normal)]
+                HM = HM.replace(normal, namefont)
+        bio = f"  {HM}"
+        LOGS.info(bio)
+        try:
+            await sython(
+                functions.account.UpdateProfileRequest(
+                    about=bio
+                )
+            )
+        except FloodWaitError as ex:
+            LOGS.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+        await asyncio.sleep(DEL_TIME_OUT)
+
 
 	
 @sython.on(events.NewMessage(outgoing=True, pattern=r".xo"))
